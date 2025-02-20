444f5a1cf30e41388e780e80598fc0d71a9f3297
aavild <anders@aavild.dk>
2025-02-20 15:01:37 +0100

    setup db for pycharm and added some working sqlite code

--- src/db.py
+++ src/db.py
@@ -1,4 +1,5 @@
 import sqlite3
+import pandas as pd
 
 def mango():
     # Create a SQL connection to our SQLite database
@@ -7,8 +8,15 @@ def mango():
     cur = con.cursor()
 
     # The result of a "cursor.execute" can be iterated over by row
-    for row in cur.execute('SELECT * FROM species;'):
+    if not cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='test';"):
+        cur.execute("CREATE TABLE test (column1 INT, column2 STRING, column3 float);")
+    cur.execute("INSERT INTO test VALUES (5, 'hey', 2.0)")
+    for row in cur.execute("SELECT * FROM test;"):
         print(row)
+
+    con.commit() # saves row from line 13
+
+    #df = pd.read_sql_query("", con)
 
     # Be sure to close the connection
     con.close()
