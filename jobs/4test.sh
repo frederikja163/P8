25bde5570ef929f668076c931d29342b84a21a41
aavild <anders@aavild.dk>
2025-05-01 16:00:30 +0200

    input all args rather than only first for all jobs

--- jobs/4test.sh
+++ jobs/4test.sh
@@ -8,4 +8,4 @@
 #SBATCH --gres=gpu:1                   # Allocated GPUs
 #SBATCH --time=01:00:00                # Maximum run time
 
-singularity exec --nv --bind ..:/P8 /ceph/container/pytorch/pytorch_25.02.sif /bin/bash /P8/scripts/run_python.sh "train.py --task test $1"
+singularity exec --nv --bind ..:/P8 /ceph/container/pytorch/pytorch_25.02.sif /bin/bash /P8/scripts/run_python.sh "train.py --task test $*"
