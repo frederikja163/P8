d9aaa444fb8413888f3cfaf923668c57f2b3cc99
aavild <anders@aavild.dk>
2025-05-21 19:11:33 +0200

    bump

--- jobs/1preprocess.sh
+++ jobs/1preprocess.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=preproces           # Name of your job
 #SBATCH --output=1preprocess.out       # Name of the output file
 #SBATCH --error=1preprocess.err        # Name of the error file
-#SBATCH --mem=24G                      # Memory
+#SBATCH --mem=70G                      # Memory
 #SBATCH --cpus-per-task=15             # CPUs per task
 #SBATCH --gres=gpu:1                   # Allocated GPUs
 #SBATCH --time=01:00:00                # Maximum run time
