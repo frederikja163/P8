c550a604747e8d56040867c052dc7a454122ecbd
aavild <anders@aavild.dk>
2025-05-22 11:50:30 +0200

    update lazy script

--- jobs/lazy_start.sh
+++ jobs/lazy_start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-job1=$(sbatch 1preprocess.sh "--dataset tdrive --grid_size 0.3" | awk '{print $4}')
-job2=$(sbatch --dependency=afterok:$job1 2generate_outliers.sh "--dataset tdrive" | awk '{print $4}')
-job3=$(sbatch --dependency=afterok:$job2 3train.sh "--dataset tdrive --batch_size 200" | awk '{print $4}')
-job4=$(sbatch --dependency=afterok:$job3 4test.sh "--dataset tdrive --batch_size 200" | awk '{print $4}')
+scancel -u $USER
+
+job1=$(sbatch 1preprocess.sh "--dataset $1 --grid_size 0.3" | awk '{print $4}')
+job2=$(sbatch --dependency=afterok:$job1 2generate_outliers.sh "--dataset $1" | awk '{print $4}')
+job3=$(sbatch --dependency=afterok:$job2 3train.sh "--dataset $1 --batch_size 200" | awk '{print $4}')
+job4=$(sbatch --dependency=afterok:$job3 4test.sh "--dataset $1 --batch_size 200" | awk '{print $4}')
