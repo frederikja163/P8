faacdade7be650aa3bfcc7d223b06c602ba787aa
aavild <anders@aavild.dk>
2025-05-22 12:36:07 +0200

    added echo of jobids

--- jobs/lazy_start.sh
+++ jobs/lazy_start.sh
@@ -4,3 +4,5 @@ job1=$(sbatch 1preprocess.sh "--dataset $1 --grid_size 0.3" | awk '{print $4}')
 job2=$(sbatch --dependency=afterok:$job1 2generate_outliers.sh "--dataset $1" | awk '{print $4}')
 job3=$(sbatch --dependency=afterok:$job2 3train.sh "--dataset $1 --batch_size 200" | awk '{print $4}')
 job4=$(sbatch --dependency=afterok:$job3 4test.sh "--dataset $1 --batch_size 200" | awk '{print $4}')
+
+echo "Started jobs $job1 $job2 $job3 $job4"
