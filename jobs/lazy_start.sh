7467bc3b07f84bc37e7b554292474280fb918548
aavild <anders@aavild.dk>
2025-05-22 13:29:04 +0200

    fix incorrect input arg

--- jobs/lazy_start.sh
+++ jobs/lazy_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-job1=$(sbatch 1preprocess.sh "--dataset $1 --grid_size 0.3 --epochs 0.1" | awk '{print $4}')
+job1=$(sbatch 1preprocess.sh "--dataset $1 --grid_size 0.3 --epoch_split 0.1" | awk '{print $4}')
 job2=$(sbatch --dependency=afterok:$job1 2generate_outliers.sh "--dataset $1" | awk '{print $4}')
 job3=$(sbatch --dependency=afterok:$job2 3train.sh "--dataset $1 --batch_size 200" | awk '{print $4}')
 job4=$(sbatch --dependency=afterok:$job3 4test.sh "--dataset $1 --batch_size 200" | awk '{print $4}')
